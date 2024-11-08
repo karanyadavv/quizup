8056110c5029f19e88090b5281f27b336cc845cf
karanyadavv <karanyadav729@gmail.com>
2024-11-08 20:35:14 +0530

    Seperated CloseButton

---
+++ src/components/CloseButtonSVG.jsx
@@ -0,0 +1,18 @@
+export default function CloseButtonSVG() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="#f4f3f6"
+      strokeWidth={1}
+      stroke="black"
+      className="size-8 top-4 right-8 md:top-14 md:right-80 absolute cursor-pointer"
+    >
+      <path
+        fillRule="evenodd"
+        d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-1.72 6.97a.75.75 0 1 0-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06L12 13.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L13.06 12l1.72-1.72a.75.75 0 1 0-1.06-1.06L12 10.94l-1.72-1.72Z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+}
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { decode } from "html-entities";
+import CloseButtonSVG from "./CloseButtonSVG";
 
 export default function Quiz() {
   const location = useLocation();
@@ -9,6 +10,7 @@ export default function Quiz() {
   let correctAnswerRef = useRef(0);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [shuffledAnswers, setShuffledAnswers] = useState([]);
+  const [isCorrect, setIsCorrect] = useState(false);
 
   const currentQuestion = questions?.[currentQuestionIndex];
 
@@ -27,10 +29,13 @@ export default function Quiz() {
     if (event.target.value === questions[currentQuestionIndex].correct_answer) {
       correctAnswerRef.current++;
       console.log("correct answer");
+      setIsCorrect(true);
     } else {
       console.log("incorrect answer");
+      setIsCorrect(false);
     }
   };
+
   const handleContinue = () => {
     setCurrentQuestionIndex((prev) => {
       if (prev === DATA_LENGTH + 1) {
@@ -41,41 +46,6 @@ export default function Quiz() {
     });
   };
 
-  function CloseButtonSVG() {
-    return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="#f4f3f6"
-        strokeWidth={1}
-        stroke="black"
-        className="size-8 top-4 right-8 md:top-14 md:right-80 absolute cursor-pointer"
-      >
-        <path
-          fillRule="evenodd"
-          d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-1.72 6.97a.75.75 0 1 0-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06L12 13.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L13.06 12l1.72-1.72a.75.75 0 1 0-1.06-1.06L12 10.94l-1.72-1.72Z"
-          clipRule="evenodd"
-        />
-      </svg>
-    );
-
-    function CheckMarkSVG() {
-      return (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          className="size-6"
-        >
-          <path
-            fillRule="evenodd"
-            d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
-            clipRule="evenodd"
-          />
-        </svg>
-      );
-    }
-  }
   return (
     <>
       <CloseButtonSVG />
@@ -98,16 +68,40 @@ export default function Quiz() {
             </p>
 
             <div className="grid grid-cols-1 gap-4">
-              {shuffledAnswers.map((answer, index) => (
-                <button
-                  key={index}
-                  onClick={handleUserAnswer}
-                  value={answer}
-                  className="w-full h-14 py-3 px-4 text-left bg-[#f4f3f6] rounded-md duration-200"
-                >
-                  {decode(answer)}
-                </button>
-              ))}
+              {shuffledAnswers.map((answer, index) => {
+                return (
+                  <>
+                    {/* <div className="w-full h-14 py-3 px-4 text-left bg-[#f4f3f6] rounded-md duration-200">
+                      <input
+                        type="radio"
+                        id="answer"
+                        name="answer"
+                        //className="opacity-0"
+                      />
+                      <label
+                        htmlFor="answer"
+                        key={index}
+                        value={answer}
+                        className=""
+                      >
+                        {decode(answer)}
+                      </label>
+                    </div> */}
+                    <button
+                      key={index}
+                      onClick={handleUserAnswer}
+                      value={answer}
+                      className={`w-full h-14 py-3 px-4 text-left bg-[#f4f3f6] rounded-md duration-200 ${
+                        isCorrect
+                          ? "active:bg-green-500 active:text-white"
+                          : "active:bg-red-500"
+                      }`}
+                    >
+                      {decode(answer)}
+                    </button>
+                  </>
+                );
+              })}
             </div>
           </div>
         )}
